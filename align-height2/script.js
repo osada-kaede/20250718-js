4c28a4c356e3aca104f07036e0ffe683eda1e9f0
osada-kaede <167516814+osada-kaede@users.noreply.github.com>
2025-07-18 18:30:41 +0900

    Add files via upload

---
+++ align-height2/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    let h=0;
+    $("main div").each(function(){
+        if($(this).height()>h){
+            h =$(this).height();
+        }
+    });
+
+    $("main div").css("height",h+"px");
+});
